e786aaebed42a5ba8c2609534d5da9a5a4aceb70
Sai-Vamsi-Ogety <ogetyvamsi@gmail.com>
2020-08-16 00:09:57 -0700

    file name changed

--- display_code_folder/app.py
+++ display_code_folder/app.py
@@ -1,7 +1,7 @@
 from flask import Flask,url_for,render_template,request
-import spacy
+import spacy as space
 from spacy import displacy
-nlp = spacy.load('en')
+nlp = spacy.load('en_core_web_sm')
 import json
 
 HTML_WRAPPER = """<div style="overflow-x: auto; border: 1px solid #e6e9ef; border-radius: 0.25rem; padding: 1rem">{}</div>"""
